87a9cb7d672d08f0ebbc867637510b1cff747da9
Kemal <1kemaladlig@gmail.com>
2026-02-06 10:14:10 +0300

    style: revert hero heading size to 7xl

--- src/components/home/hero.tsx
+++ src/components/home/hero.tsx
@@ -28,7 +28,7 @@ export default function Hero() {
                         Available for new projects
                     </div>
 
-                    <h1 className="text-4xl md:text-8xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-600 dark:from-neutral-50 dark:to-neutral-400 bg-opacity-50 font-heading tracking-tight pb-4">
+                    <h1 className="text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-b from-neutral-900 to-neutral-600 dark:from-neutral-50 dark:to-neutral-400 bg-opacity-50 font-heading tracking-tight pb-4">
                         Crafting High-Performance <br /> Mobile & Cloud Solutions.
                     </h1>
                     <p className="mt-4 font-normal text-lg text-neutral-600 dark:text-neutral-300 max-w-2xl mx-auto leading-relaxed">
